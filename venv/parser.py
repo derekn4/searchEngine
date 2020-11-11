4bec1c98cd8d6eb10eb99ccb5cdb16ae97038622
Derek Nguyen <derek.nguyen99@gmail.com>
2020-11-10 20:31:34 -0800

    count json files

--- venv/parser.py
+++ venv/parser.py
@@ -3,5 +3,14 @@ from bs4 import BeautifulSoup
 import nltk
 from nltk import word_tokenize
 
-def parse(corpusPath):
-    corpus_size = len([direct for direct in os.listdir(corpusPath) if direct.endswith(".txt")])
+#def parse(corpusPath):
+#    corpus_size = len([direct for direct in os.listdir(corpusPath) if direct.endswith(".txt")])
+
+print(os.listdir("DEV"))
+count = 0
+for direct in os.listdir("DEV"):
+    for i in os.listdir("DEV\\" + direct):
+        if i.endswith(".json"):
+            count+=1
+
+print(count)
