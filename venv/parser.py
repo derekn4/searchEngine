d7b0b23e71ac056238d536da7d88b259ddad5c7a
Alex Meng <alexmeng99@gmail.com>
2020-11-10 20:45:44 -0800

    Porter Stemmer set up
    import porterstemmer from nltk & ps obj

--- venv/parser.py
+++ venv/parser.py
@@ -1,11 +1,15 @@
 import os, json
 from bs4 import BeautifulSoup
 import nltk
-from nltk import word_tokenize
+from nltk import word_tokenizesent_tokenize
+from nltk.stem import PorterStemmer
 
 #def parse(corpusPath):
 #    corpus_size = len([direct for direct in os.listdir(corpusPath) if direct.endswith(".txt")])
 #hey there
+#use nltk to stem works
+
+ps = PorterStemmer()
 
 print(os.listdir("DEV"))
 count = 0
