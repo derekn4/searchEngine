ed583c6eec224cdf22a0f1793f010e14015a5ffc
Derek Nguyen <derek.nguyen99@gmail.com>
2020-11-11 13:38:12 -0800

    Tokenize with Dictionary + DocID dictionary

--- venv/parser.py
+++ venv/parser.py
@@ -4,13 +4,32 @@ from bs4 import BeautifulSoup
 import nltk
 from nltk import word_tokenize, sent_tokenize
 
+stop_words = ['about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are', "aren",
+              'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by',
+              "can", 'cannot', 'could', "couldn", 'did', "didn", 'do', 'does', "doesn", 'doing', "don",
+              'down', 'during', 'each', 'few', 'for', 'from', 'further', 'had', "hadn", 'has', "hasn", 'have',
+              "haven't", 'having', 'he', 'her', 'here', 'hers', 'herself', 'him',
+              'himself', 'his', 'how', "how's", 'if', 'in', 'into', 'is',
+              "isn", 'it', 'its', 'itself', 'me', 'more', 'most', "mustn", 'my', 'myself',
+              'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'ought', 'our',
+              'ours', 'ourselves', 'out', 'over', 'own', 'same', "shan't", 'she',
+              'should', "shouldn", 'so', 'some', 'such', 'than', 'that', "that's", 'the', 'their', 'theirs',
+              'them', 'themselves', 'then', 'there', 'these', 'they',
+              'this', 'those', 'through', 'to', 'too', 'under', 'until', 'up', 'very', 'was', "wasn",
+              'we', 'were', "weren", 'what', 'when',
+              'where', 'which', 'while', 'who', 'whom', 'why', 'with', "won",
+              'would', "wouldn", 'you', "ll", "re", "ve", 'your', 'yours', 'yourself',
+              'yourselves']
 
+word_dict = dict()
+docID_dict = dict()
 #def parse(corpusPath):
 #    corpus_size = len([direct for direct in os.listdir(corpusPath) if direct.endswith(".txt")])
 
 
 #Count size of Corpus
 #Make dictiornary of number: json file
+#Make list of Token : [[docIDs where found], tf-dif]
 def get_tokens(content, words):
     tokens = []
     for t in content.text.split():
@@ -25,18 +44,32 @@ def get_tokens(content, words):
 
 token_list = []
 count = 0
+
+
 for direct in os.listdir("DEV"):
     for i in os.listdir("DEV\\" + direct):
         if i.endswith(".json"):
+            file = "DEV\\" + direct + "\\" + i  # get file Path
+            data = json.load(open(file))  # json.load file makes dict of file data
+            docID_dict[count] = data["url"]
             count+=1                            #increment corpus Size
-            file = "DEV\\" + direct + "\\" + i  #get file Path
-            data = json.load(open(file))        #json.load file makes dict of file data
-            print(data['content'])
+            #print(data["url"])
+            #print(data['content'])
             content = BeautifulSoup(data['content'], "lxml")
-            print(content)
+            #print(content)
             tokenized_text = get_tokens(content, token_list)
-            print(tokenized_text)
+            #print(tokenized_text)
+            for word in tokenized_text:
+                if word not in stop_words:
+                    if word not in word_dict.keys():
+                        word_dict[word] = 1
+                    else:
+                        word_dict[word] += 1
+            if count==200:
+                break
+    if count==200:
         break
-    break
 
-print(count)
+sort_dict = {k: v for k, v in sorted(word_dict.items(), key=lambda item: item[1], reverse=True)}
+print(docID_dict)
+print(sort_dict)
