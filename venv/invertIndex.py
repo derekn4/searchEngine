737101b22d7b2f7c83d96baba3c80d0871bda67f
Derek Nguyen <32850977+derekn4@users.noreply.github.com>
2020-11-17 14:21:28 -0800

    Update invertIndex.py

--- venv/invertIndex.py
+++ venv/invertIndex.py
@@ -29,81 +29,96 @@ index_list = []
 def get_tokens(content):
     tokens = []
     words = []
-    for t in content.text.split():
+    for t in content.split():
         tokens = re.split(r'[^a-z0-9]+', t.lower())
         words.extend(tokens)
-
     for word in words:
-        if len(word) >= 2 and not word.isdigit():
-            tokens += [word]
+         if len(word) >= 2 and not word.isdigit():
+             tokens += [word]
     return tokens
-
 
 def count_freq_url(tokens):
     word_dict1 = dict()
     for word in tokens:
-        if word not in word_dict1.keys():
+        if sno.stem(word) not in word_dict1.keys():
             if word != '':
-                #word_dict1[word] = 1
                 word_dict1[sno.stem(word)] = 1
         else:
             if word != '':
-                # word_dict1[word] += 1
-                if sno.stem(word) in word_dict1.keys() and sno.stem(word) != word:
-                    word_dict1[sno.stem(word)] += 1
-                else:
-                    word_dict1[sno.stem(word)] = 1
+                word_dict1[sno.stem(word)] += 1
 
-        if word in doc_freq.keys():
-            #doc_freq[word] += 1
-            if sno.stem(word) in doc_freq.keys() and sno.stem(word) != word:
+        if sno.stem(word) in doc_freq.keys():
+            if word!="":
                 doc_freq[sno.stem(word)] += 1
-            else:
-                doc_freq[sno.stem(word)] = 1
         else:
-            #doc_freq[word] = 1
-            doc_freq[sno.stem(word)] = 1
-
+            if word!="":
+                doc_freq[sno.stem(word)] = 1
     return word_dict1
+# def count_freq_url(tokens):
+#     word_dict1 = dict()
+#     for word in tokens:
+#         if word not in word_dict1.keys():
+#             if word != '':
+#                 #word_dict1[word] = 1
+#                 word_dict1[sno.stem(word)] = 1
+#         else:
+#             if word != '':
+#                 # word_dict1[word] += 1
+#                 if sno.stem(word) in word_dict1.keys() and sno.stem(word) != word:
+#                     word_dict1[sno.stem(word)] += 1
+#                 else:
+#                     word_dict1[sno.stem(word)] = 1
+#
+#         if word in doc_freq.keys():
+#             #doc_freq[word] += 1
+#             if sno.stem(word) in doc_freq.keys() and sno.stem(word) != word:
+#                 doc_freq[sno.stem(word)] += 1
+#             else:
+#                 doc_freq[sno.stem(word)] = 1
+#         else:
+#             #doc_freq[word] = 1
+#             doc_freq[sno.stem(word)] = 1
+#
+#     return word_dict1
 
-def mergeIndex(temp, index):
-    temp_index = open(temp, "r")
-    temp2 = open("Store/temp_index2.txt", "a+")
-    temp2.truncate(0)
-    index_file = open(index, "w")
-    temp_list = []
-    temp2_list = []
-    count = 0
-    merge = defaultdict(list)
-
-    for t in temp_index:
-        res = ast.literal_eval(t)
-        temp_list.append(res)
-    # print(len(temp_list))
-
-    merge = temp_list[0]
-    while count < len(temp_list):
-        count += 1
-        for k, v in chain(merge.items(), temp_list[count].items()):
-            if k not in merge.keys():
-                merge[k] = v
-            else:
-                merge[k].extend(v)
-        if count % 15 == 0:
-            temp2.write(str(merge) + "\n")
-            merge = dict()
-        if count == len(temp_list) - 1:
-            temp2.write(str(merge) + "\n")
-            merge = dict()
-
-    for t in temp2:
-        res = ast.literal_eval(t)
-        temp2_list.append(res)
-
-    # merge = dict(merge)
-    # index_file.write(str(merge) + "\n")
-    # index_file.close()
-    temp_index.close()
+# def mergeIndex(temp, index):
+#     temp_index = open(temp, "r")
+#     temp2 = open("Store/temp_index2.txt", "a+")
+#     temp2.truncate(0)
+#     index_file = open(index, "w")
+#     temp_list = []
+#     temp2_list = []
+#     count = 0
+#     merge = defaultdict(list)
+#
+#     for t in temp_index:
+#         res = ast.literal_eval(t)
+#         temp_list.append(res)
+#     # print(len(temp_list))
+#
+#     merge = temp_list[0]
+#     while count < len(temp_list):
+#         count += 1
+#         for k, v in chain(merge.items(), temp_list[count].items()):
+#             if k not in merge.keys():
+#                 merge[k] = v
+#             else:
+#                 merge[k].extend(v)
+#         if count % 15 == 0:
+#             temp2.write(str(merge) + "\n")
+#             merge = dict()
+#         if count == len(temp_list) - 1:
+#             temp2.write(str(merge) + "\n")
+#             merge = dict()
+#
+#     for t in temp2:
+#         res = ast.literal_eval(t)
+#         temp2_list.append(res)
+#
+#     # merge = dict(merge)
+#     # index_file.write(str(merge) + "\n")
+#     # index_file.close()
+#     temp_index.close()
 
 
 def BuildIndex(tks, file, corpusSize, docID, count):
@@ -151,22 +166,46 @@ def ParseCorpus(jsonFiles):
             doc_urls["doc" + str(count)] = items["url"]
 
             content = BeautifulSoup(items["content"], "lxml")
-            tokenized_text = get_tokens(content)
+            tokenized_text = get_tokens(content.text.lower())
 
             token_dict = count_freq_url(tokenized_text)
 
-            sort_dict = {k: v for k, v in sorted(token_dict.items(), key=lambda item: item[0], reverse=True)}
+            sort_dict = {k: v for k, v in sorted(token_dict.items(), key=lambda item: item[0].lower())}
             BuildIndex(sort_dict, 'Store/index' + str(index_file_count) + '.txt', corpus_size, "doc" + str(count), count)
 
             if count % 100 == 0:
                 print(count)
         except:
             continue
-    with open("Store/docfrequencies.txt", "w") as g:
+    with open("Store/docfrequencies2.txt", "w") as g:
         g.write(str(doc_freq))
-    with open("Store/docurls.txt", "w") as h:
+    with open("Store/docurls2.txt", "w") as h:
         h.write(str(doc_urls))
 
+def queryDatabase(index_list):
+    query = input("Search the index(type quit to exit): ")
+    
+    if query=="quit":
+        exit()
+
+    search = query.split()
+
+    query_dict = dict()
+    for database in index_list:
+        file = open(database, "r")
+        for t in file:
+            res = ast.literal_eval(t)
+
+        for q in search:
+            if q in res.keys():
+                if q not in query_dict.keys():
+                    query_dict[q] = res[q]
+                else:
+                    query_dict[q].extend(res[q])
+
+    for k,v in query_dict.items():
+        #find top 5 querys where BOTH are in
+        pass
 
 tempindexfile = "Store/temp_index.txt"
 indexfile = "Store/index.txt"
